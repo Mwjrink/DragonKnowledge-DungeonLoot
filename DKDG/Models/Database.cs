f3ffdcdde1fbea20470b9116c6cf38d476879a96
Max Rink <mwjrink@gmail.com>
2018-09-30 21:42:43 -0400

    Database Saving WIP 2

---
+++ DKDG/Models/Database.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Reflection;
+using DKDG.Utils;
+
+namespace DKDG.Models
+{
+    public class Database
+    {
+        #region Fields
+
+        private readonly string path = "";
+        private readonly Dictionary<long, Armor> loadedArmor = new Dictionary<long, Armor>();
+        private readonly Dictionary<long, Background> loadedBackground = new Dictionary<long, Background>();
+        private readonly Dictionary<long, Character> loadedCharacter = new Dictionary<long, Character>();
+        private readonly Dictionary<long, Class> loadedClass = new Dictionary<long, Class>();
+        private readonly Dictionary<long, Language> loadedLanguage = new Dictionary<long, Language>();
+        private readonly Dictionary<long, Monster> loadedMonster = new Dictionary<long, Monster>();
+        private readonly Dictionary<long, NPC> loadedNPC = new Dictionary<long, NPC>();
+        private readonly Dictionary<long, OtherEquipment> loadedOtherEquipment = new Dictionary<long, OtherEquipment>();
+        private readonly Dictionary<long, Race> loadedRace = new Dictionary<long, Race>();
+        private readonly Dictionary<long, Skill> loadedSkill = new Dictionary<long, Skill>();
+        private readonly Dictionary<long, Spell> loadedSpells = new Dictionary<long, Spell>();
+        private readonly Dictionary<long, Tool> loadedTool = new Dictionary<long, Tool>();
+        private readonly Dictionary<long, Weapon> loadedWeapon = new Dictionary<long, Weapon>();
+        public static readonly long DEFAULT_ID = -1;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Database(string path)
+        {
+            this.path = path;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void GenericFillDict<T>()
+        {
+            //get dict to load into
+        }
+
+        private ISaveable Load<T>(long ID) where T : new()
+        {
+            //get properties in class and load
+            T loaded = Activator.CreateInstance<T>();
+
+            typeof(T).GetProperties();
+
+            IEnumerable<PropertyInfo> properties = typeof(T).GetProperties().Where(p => p.IsDefined(typeof(SQLPropAttribute), false));
+        }
+
+        private bool Save<T>(IEnumerable<T> items, long parentId = -1, SQLPropSaveType savingAs = SQLPropSaveType.Value) where T : ISaveable
+        {
+            IEnumerable<PropertyInfo> properties = typeof(T).GetProperties().Where(p => p.IsDefined(typeof(SQLPropAttribute), false));
+            foreach (T item in items)
+            {
+                string command = "";
+                var parameters = new List<SQLiteParameter>();
+                foreach (PropertyInfo prop in properties)
+                {
+                    var attribute = (SQLPropAttribute)prop.GetCustomAttributes(typeof(SQLPropAttribute), false).FirstOrDefault();
+                    object value = prop.GetValue(item);
+
+                    if (value == null && !attribute.Nullable)
+                        throw new NullReferenceException(item + ", " + prop.Name);
+
+                    switch (attribute.SaveRelationship)
+                    {
+                        case SQLPropSaveType.Link:
+
+                            break;
+
+                        case SQLPropSaveType.MultipleChild: //TODO Max, uh
+                            typeof(Database).GetMethod("Save").MakeGenericMethod(prop.PropertyType).Invoke(this, new object[] { new[] { value }, item.ID, SQLPropSaveType.MultipleChild });
+                            Save<prop.PropertyType>(value.Yield(), item.ID, SQLPropSaveType.MultipleChild);
+                            break;
+
+                        case SQLPropSaveType.MultipleParent:
+
+                            break;
+
+                        case SQLPropSaveType.Value:
+
+                            break;
+                    }
+                }
+
+                SQLHelper.ExecuteNonQuery(command, parameters, path);
+            }
+        }
+
+        private IEnumerable<(string command, SQLiteParameterCollection parameters)> GetSaveCommand<T>(T item)
+        {
+            IEnumerable<PropertyInfo> properties = typeof(T).GetProperties().Where(p => p.IsDefined(typeof(SQLPropAttribute), false));
+            foreach (PropertyInfo prop in properties)
+            {
+                var attribute = (SQLPropAttribute)prop.GetCustomAttributes(typeof(SQLPropAttribute), false).FirstOrDefault();
+                object value = prop.GetValue(item);
+
+                if (value == null && !attribute.Nullable)
+                    throw new NullReferenceException(item + ", " + prop.Name);
+            }
+        }
+
+        private IEnumerable<(string command, SQLiteParameterCollection parameters)> GetLoadCommand<T>(T item)
+        {
+            IEnumerable<PropertyInfo> properties = typeof(T).GetProperties().Where(p => p.IsDefined(typeof(SQLPropAttribute), false));
+            foreach (PropertyInfo prop in properties)
+            {
+                var attribute = (SQLPropAttribute)prop.GetCustomAttributes(typeof(SQLPropAttribute), false).FirstOrDefault();
+            }
+        }
+
+        //using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=D:\test.db;"))
+        //{
+        //    conn.Open();
+
+        // SQLiteCommand command = new SQLiteCommand("Select * from yourTable", conn);
+        // SQLiteDataReader reader = command.ExecuteReader();
+
+        // while (reader.Read()) Console.WriteLine(reader["YourColumn"]);
+
+        //    reader.Close();
+        //}
+
+        #endregion Methods
+    }
+}
--- DKDG/Utils/SavingLoading/SQL/SQLHelper.cs
+++ DKDG/Utils/SavingLoading/SQL/SQLHelper.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
 
-namespace DKDG.Utils.SavingLoading.SQL
+namespace DKDG.Utils
 {
     public static class SQLHelper
     {
         #region Methods
 
-        private static T CommandSurround<T>(string query,
+        private static T CommandPrep<T>(string query,
                     IEnumerable<(string parameterName, DbType parameterType, int parameterSize, object value)> sqlParameterCollection,
-                    Func<SQLiteCommand, T> action)
+                    Func<SQLiteCommand, T> action, string path = null)
         {
             try
             {
-                using (var conn = new SQLiteConnection(INIValuesStatic.DB_PATH))
+                using (var conn = new SQLiteConnection(path ?? INIValuesStatic.DB_PATH))
                 {
                     conn.Open();
 
@@ -33,21 +33,42 @@ namespace DKDG.Utils.SavingLoading.SQL
             }
         }
 
+        private static T CommandPrep<T>(string query,
+                    SQLiteParameterCollection sqlParameterCollection,
+                    Func<SQLiteCommand, T> action, string path = null)
+        {
+            try
+            {
+                using (var conn = new SQLiteConnection(path ?? INIValuesStatic.DB_PATH))
+                {
+                    conn.Open();
+
+                    var command = new SQLiteCommand(query, conn);
+                    command.Parameters.Add(sqlParameterCollection);
+                    return action.Invoke(command);
+                }
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+
         /// <summary>
         /// Execute the command and return the number of rows inserted/updated affected by it.
         /// </summary>
         /// <param name="query">The command to execute.</param>
         /// <param name="sqlParameterCollection">The parameters of the command.</param>
         /// <returns>The number of rows inserted/updated affected by it.</returns>
-        public static int ExecuteNonQuery(string query, IEnumerable<(string parameterName, DbType parameterType, int parameterSize, object value)> sqlParameterCollection)
+        public static int ExecuteNonQuery(string query, IEnumerable<(string parameterName, DbType parameterType, int parameterSize, object value)> sqlParameterCollection, string path = null)
         {
-            return CommandSurround(query, sqlParameterCollection, command => command.ExecuteNonQuery());
+            return CommandPrep(query, sqlParameterCollection, command => command.ExecuteNonQuery(), path);
         }
 
         public static DataTable ExecuteQuery(string query,
-            IEnumerable<(string parameterName, DbType parameterType, int parameterSize, object value)> sqlParameterCollection)
+            IEnumerable<(string parameterName, DbType parameterType, int parameterSize, object value)> sqlParameterCollection, string path = null)
         {
-            return CommandSurround(query, sqlParameterCollection, command =>
+            return CommandPrep(query, sqlParameterCollection, command =>
                         {
                             using (SQLiteDataReader reader = command.ExecuteReader())
                             {
@@ -59,7 +80,30 @@ namespace DKDG.Utils.SavingLoading.SQL
 
                                 return result;
                             }
-                        });
+                        }, path);
+        }
+
+        public static int ExecuteNonQuery(string query, SQLiteParameterCollection sqlParameterCollection, string path = null)
+        {
+            return CommandPrep(query, sqlParameterCollection, command => command.ExecuteNonQuery(), path);
+        }
+
+        public static DataTable ExecuteQuery(string query,
+            SQLiteParameterCollection sqlParameterCollection, string path = null)
+        {
+            return CommandPrep(query, sqlParameterCollection, command =>
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    var result = new DataTable();
+
+                    result.Load(reader);
+
+                    reader.Close();
+
+                    return result;
+                }
+            }, path);
         }
 
         #endregion Methods
